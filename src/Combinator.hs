28502c4ab67f99c5045c337d5b24291eeb2ec804
Spencer Ward <SpencerArtisan>
2014-10-19 20:14:35 +0100

    Add another where

--- src/Combinator.hs
+++ src/Combinator.hs
@@ -3,7 +3,8 @@ module Combinator (combinations, subsets) where
 combinations :: [Int] -> [[Int]]
 combinations [] = []
 combinations [x] = [[x]]
-combinations xs = [(xs!!i) : aa | i <- [0..length xs - 1], aa <- combinations $ (take i xs ++ drop (i+1) xs)]
+combinations xs = [(xs!!i) : aa | i <- [0..length xs - 1], aa <- combinations $ cutOut i xs]
+    where cutOut i xs = take i xs ++ drop (i+1) xs
 
 subsets :: [Int] -> [[Int]]
 subsets [] = []
