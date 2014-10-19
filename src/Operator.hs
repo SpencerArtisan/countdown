f6e2ff981f0f675d4e7bfdf756c8b9d4dd4c8102
Spencer Ward <SpencerArtisan>
2014-10-19 22:48:14 +0100

    Use _ for unused params

--- src/Operator.hs
+++ src/Operator.hs
@@ -1,19 +1,19 @@
 module Operator (operate, multiOperate, operateWithMemo, plus, minus, times) where
 
 operate :: (Int -> Int -> Int) -> [Int] -> Int
-operate f [] = 0
+operate _ [] = 0
 operate f xs = foldl1 f xs
 
 multiOperate:: [(Int -> Int -> Int)] -> [Int] -> [Int]
-multiOperate ops [x] = [x]
+multiOperate _ [x] = [x]
 multiOperate ops (x:y:zs) = 
   concat [multiOperate ops (operate op [x,y]:zs) | op <- ops]
 
 operateWithMemo :: [(Int,String) -> (Int,String) -> (Int, String)] -> [(Int,String)] -> [(Int,String)]
-operateWithMemo ops [] = []
-operateWithMemo ops [x] = [x]
+operateWithMemo _ [] = []
+operateWithMemo _ [x] = [x]
 operateWithMemo ops (x:y:zs) = 
-  concat [operateWithMemo ops ((op x y):zs) | op <- ops]
+  concat [operateWithMemo ops (op x y:zs) | op <- ops]
 
 plus :: (Int,String) -> (Int,String) -> (Int, String)
 plus = applyOp (+) "+"
