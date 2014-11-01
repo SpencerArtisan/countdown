90386d5b8b619959725a41cf16b0d649621d07ce
Spencer Ward <SpencerArtisan>
2014-11-01 20:02:29 +0000

    method to return all solutions

--- src/Countdown.hs
+++ src/Countdown.hs
@@ -1,18 +1,26 @@
-module Countdown (countdown, countdownTarget) where
+module Countdown (countdown, countdownTarget, countdownTargets) where
 
 import Combinator
 import Operator
+import Data.List
 
 countdown :: [Int] -> [Solution]
 countdown [] = []
 countdown xs = concat [operateWithMemo ops (map toSolution cm) | ss <- subsets xs, cm <- combinations ss]
     where toSolution c = Solution c $ show c
-          ops = [plus, minus, times]
+          ops = [plus, minus, times, divide]
 
 countdownTarget :: [Int] -> Int -> String
 countdownTarget xs t 
     | null calcs = "No answer"
     | otherwise  = let Solution x s = head calcs in s
+    where hitsTarget (Solution v r) = t == v
+          calcs = filter hitsTarget $ countdown xs
+
+countdownTargets :: [Int] -> Int -> [String]
+countdownTargets xs t 
+    | null calcs = []
+    | otherwise  = map (\(Solution x s) -> s) calcs
     where hitsTarget (Solution v r) = t == v
           calcs = filter hitsTarget $ countdown xs
 
--- src/Operator.hs
+++ src/Operator.hs
@@ -1,4 +1,4 @@
-module Operator (Solution(..), operateWithMemo, plus, minus, times) where
+module Operator (Solution(..), operateWithMemo, plus, minus, times, divide) where
 
 data Solution = Solution Int String deriving (Show,Eq)
 
@@ -18,6 +18,12 @@ times :: Solution -> Solution -> Solution
 times (Solution x s)
     | any (`elem` "+-") s = applyOp (*) "*" $ Solution x $ "(" ++ s ++ ")"
     | otherwise           = applyOp (*) "*" $ Solution x s
+
+divide :: Solution -> Solution -> Solution
+divide (Solution x s1) (Solution y s2)
+    | x `mod` y /= 0       = Solution x s1 
+    | any (`elem` "+-") s1 = applyOp (div) "/" (Solution x $ "(" ++ s1 ++ ")") (Solution y s2)
+    | otherwise            = applyOp (div) "/" (Solution x s1) (Solution y s2)
 
 applyOp :: (Int -> Int -> Int) -> String -> Solution -> Solution -> Solution
 applyOp f sym (Solution x s1) (Solution y s2) = Solution (f x y) (concat [s1,sym,s2])
